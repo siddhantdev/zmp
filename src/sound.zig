4d1c2456e8996650ee6c7e4a7d2eb09c3efd9912
siddhantdev <agarwalsiddhant53@gmail.com>
2026-02-08 15:46:45 +0530

    Use optional type to store sound state
    Instead of managing the sound state manually with a boolean, use Zig's
    optional type to do it. Makes it easier to maintain and reduces changes
    of bugs due to improper state management.

--- src/sound.zig
+++ src/sound.zig
@@ -4,8 +4,7 @@ const ma = @cImport({
 });
 
 var engine: ma.ma_engine = undefined;
-var sound: ma.ma_sound = undefined;
-var sound_init = false;
+var sound: ?ma.ma_sound = null;
 var sound_name: [*:0]u8 = undefined;
 
 pub fn init_engine() !void {
@@ -26,35 +25,43 @@ pub fn play_file(sound_file: [*:0]u8) !void {
     deinit_sound();
     var result: ma.ma_result = undefined;
 
-    result = ma.ma_sound_init_from_file(&engine, sound_file, 0, null, null, &sound);
+    sound = undefined;
+    result = ma.ma_sound_init_from_file(&engine, sound_file, 0, null, null, &sound.?);
     if (result != ma.MA_SUCCESS) {
         return error.CouldNotLoadSound;
     }
-    sound_init = true;
     sound_name = sound_file;
 
-    _ = ma.ma_sound_start(&sound);
+    _ = ma.ma_sound_start(&sound.?);
 }
 
 pub fn deinit_sound() void {
-    if (sound_init) {
-        ma.ma_sound_uninit(&sound);
-        sound_init = false;
-        sound_name = undefined;
+    if (sound == null) {
+        return;
     }
+
+    ma.ma_sound_uninit(&sound.?);
+    sound = null;
+    sound_name = undefined;
 }
 
 pub fn get_sound_done() f32 {
-    if (!sound_init) return 0;
+    if (sound == null) {
+        return 0;
+    }
+
     var done: f32 = 0;
-    _ = ma.ma_sound_get_cursor_in_seconds(&sound, &done);
+    _ = ma.ma_sound_get_cursor_in_seconds(&sound.?, &done);
     return done;
 }
 
 pub fn get_sound_len() f32 {
-    if (!sound_init) return 0;
+    if (sound == null) {
+        return 0;
+    }
+
     var len: f32 = 0;
-    _ = ma.ma_sound_get_length_in_seconds(&sound, &len);
+    _ = ma.ma_sound_get_length_in_seconds(&sound.?, &len);
     return len;
 }
 
@@ -63,5 +70,9 @@ pub fn get_sound_name() []u8 {
 }
 
 pub fn is_sound_finished() bool {
-    return (ma.ma_sound_at_end(&sound) == 1);
+    if (sound == null) {
+        return false;
+    }
+
+    return (ma.ma_sound_at_end(&sound.?) == 1);
 }
